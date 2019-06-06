1e2e070e705c972b0b85b7fd081a3ec77bb155ab
Dennise <denimoji@gmail.com>
2019-06-05 20:30:48 -0500

    Actualización de rutas
    olvide modificar algunas rutas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   loginPoint: 'https://auth-service-huachicol.herokuapp.com/oauth/token',
-  reportPoint: 'https://complain-service-huachicol.herokuapp.com/api/v1/user/center/coordinates',
-  ductosPoint: 'https://complain-service-huachicol.herokuapp.com/api/v1/user/ductos/page/0',
-  assignPoint: 'https://complain-service-huachicol.herokuapp.com/api/v1/user/center/coordinates',
+  reportPoint: 'https://damp-ocean-98658.herokuapp.com/api/v1/user/center/coordinates',
+  ductosPoint: 'https://damp-ocean-98658.herokuapp.com/api/v1/user/ductos/page/0',
+  assignPoint: 'https://damp-ocean-98658.herokuapp.com/api/v1/user/center/coordinates',
   userAuth: 'angularjwtclientid',
   passAuth: '12345',
   userRole: 'CIUDADANO_ROLE',
   adminRole: 'ADMINISTRADOR_ROLE',
   invalidToken: 'invalid_token',
-  crearDenuncia: 'https://complain-service-huachicol.herokuapp.com/api/v1/user/complain/',
+  crearDenuncia: 'https://damp-ocean-98658.herokuapp.com/api/v1/user/complain/',
   mostrarDenuncia: 'https://damp-ocean-98658.herokuapp.com/api/v1/user/complains/',
   registroUsuario: 'https://auth-service-huachicol.herokuapp.com/api/v1/ciudadano'
 };
